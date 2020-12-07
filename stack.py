d0a989890bc03d258e4a780d8ba183b5bf8eb75e
Ian Snyder <isnide23@gmail.com>
2020-12-06 21:19:47 -0800

    forgot to push this a long time ago

--- stack.py
+++ stack.py
@@ -1,7 +1,33 @@
 # Stack: A stack.
 # Your implementation should pass the tests in stack.py.
-# YOUR NAME
+# Ian Snyder
 
 class Stack:
 
-    pass
+    def __init__(self):
+        self.items = []
+        self.size = 0
+
+    def is_empty(self):
+        if self.size == 0:
+            return True
+        else:
+            return False
+
+    def pop(self):
+        if self.is_empty() is True:
+            raise IndexError
+        else:
+            x = self.items.pop(self.size - 1)
+            self.size -= 1
+            return x
+
+    def peek(self):
+        if self.is_empty() is True:
+            raise IndexError
+        else:
+            return self.items[self.size - 1]
+
+    def push(self, item):
+        self.items.append(item)
+        self.size += 1
